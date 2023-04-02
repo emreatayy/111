acb37cb49406e6ba4d844ab79d059769a25c7753
Yunus EMRE Atay <yns.emreatay@gmail.com>
2023-04-02 16:30:28 +0300

    updt

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function my_computer(x) {
+    close_window();
     var my_computer = document.getElementById("window");
     var my_content = document.getElementById(x);
     my_computer.style.display = "flex";
     my_content.style.display = "flex"
+}
+function my_computer2(x) {
+    var all_content = document.getElementsByClassName("content");
+    var my_content = document.getElementById(x);
+    for (let index = 0; index < all_content.length; index++) {
+        all_content[index].style.display = "none";    
+    }
+    my_content.style.display = "flex";
 }
 
 function close_window() {
